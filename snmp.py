88018ffc40c265ed1c0ef97402b29bc63aef9568
Kristoffer Dalby <kradalby@kradalby.no>
2015-04-19 12:08:24 +0200

    added a getwhatwewant method

--- snmp.py
+++ snmp.py
@@ -44,18 +44,72 @@ class SNMP():
                 for name, val in varBinds:
                     return "%s" % name.prettyPrint(), "%s" % val.prettyPrint()
 
-    def get_bulk_OID_name_value(self, oidlist):
+    def get_bulk_OID_name_value(self, oid, depth):
         '''
-        Get a dictionary with names as keys and value as value
+        Get a name, value pair from a OID
 
-        returns: { OID: value }
+        returns: name, value
         '''
-        result = {}
-        for oid in oidlist:
-            name, value = self.get_OID_name_value(oid)
-            result[name] = value
-        return result
+        errorIndication, errorStatus, errorIndex, varBindTable = self.cmd.bulkCmd(
+            cmdgen.CommunityData(self.community),
+            cmdgen.UdpTransportTarget((self.address, self.port)),
+            0, depth,
+            oid
+        )
+
+        if errorIndication:
+            print(errorIndication)
+        else:
+            if errorStatus:
+                print('%s at %s' % (
+                    errorStatus.prettyPrint(),
+                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
+                    )
+                )
+            else:
+                #print varBindTable
+
+                data = []
+
+                for varBindTableRow in varBindTable:
+                    for name, val in varBindTableRow:
+                        data.append(("%s" % name.prettyPrint(), "%s" % val.prettyPrint()))
+                return data
+
+    #def get_bulk_OID_name_value(self, oidlist):
+    #    '''
+    #    Get a dictionary with names as keys and value as value
+
+    #    returns: { OID: value }
+    #    '''
+    #    result = {}
+    #    for oid in oidlist:
+    #        name, value = self.get_OID_name_value(oid)
+    #        result[name] = value
+    #    return result
 
 
+    def get_what_we_want(self):
+        ipaddress = "1.3.6.1.2.1.4.20.1.1"
+        mask = "1.3.6.1.2.1.4.20.1.3"
+        ifname = "1.3.6.1.2.1.2.2.1.2"
+
+        _, number_of_interfaces = self.get_OID_name_value("1.3.6.1.2.1.2.1.0")
+        _, os = self.get_OID_name_value("1.3.6.1.2.1.1.1.0")
+
+        ipaddresses = self.get_bulk_OID_name_value(ipaddress, 1)
+        masks = self.get_bulk_OID_name_value(mask, 1)
+        ifnames = self.get_bulk_OID_name_value(ifname, 1)
+
+        data = []
+
+        for i in range(int(number_of_interfaces)):
+            data.append({
+                "name": ifnames[i][1],
+                "ip": ipaddresses[i][1],
+                "mask": masks[i][1],
+            })
+
+        return os, data
 
 
