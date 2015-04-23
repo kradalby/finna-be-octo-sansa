bc6003ada5dbf3bfed8ee3ae8385645c8cda9274
Kristoffer Dalby <kradalby@kradalby.no>
2015-04-23 14:14:24 +0200

    lolwhat?

--- snmp.py
+++ snmp.py
@@ -81,21 +81,28 @@ class SNMP():
 
     def get_what_we_want(self):
         '''
+        Get all the information needed for the assignment.
 
+        returns: string with os version information and a list with dictionaries with interface information.
         '''
+
+        # Create variables for the used OIDs
         ipaddress = "1.3.6.1.2.1.4.20.1.1"
         mask = "1.3.6.1.2.1.4.20.1.3"
         ifname = "1.3.6.1.2.1.2.2.1.2"
 
+        # Get the number of interfaces and OS information
         _, number_of_interfaces = self.get_OID_name_value("1.3.6.1.2.1.2.1.0")
         _, os = self.get_OID_name_value("1.3.6.1.2.1.1.1.0")
 
+        # Get the details about the network interfaces
         ipaddresses = self.get_bulk_OID_name_value(ipaddress, 1)
         masks = self.get_bulk_OID_name_value(mask, 1)
         ifnames = self.get_bulk_OID_name_value(ifname, 1)
 
         data = []
 
+        # Format the data in the way it is expected to return
         for i in range(int(number_of_interfaces)):
             data.append({
                 "name": ifnames[i][1],
